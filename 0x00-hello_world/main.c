921521889f030c91166d133b985a64e8c9007c1f
Neo Metsing <n.metsing00@gmail.com>
2022-09-08 08:58:22 -0700

    using gcc

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -1,19 +1,21 @@
-942 "/usr/include/stdio.h" 3 4
+include <stdio.h>
 
 
 
-# 2 "main.c" 2
-
-
-
-
-
-# 3 "main.c"
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
 
 int main(void)
 
 {
-  
-  return (0);
-  
+
+	    return (0);
+
 }
