8e4d94c22020d5e10a2fb7739e56b2fd52879057
Neo Metsing <n.metsing00@gmail.com>
2022-09-08 08:43:29 -0700

    added a copy paste

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -1,16 +1,17 @@
-#include <stdio.h.>
-/**
+942 "/usr/include/stdio.h" 3 4
 
- * main - Entry point
 
- *
 
- * Return: Always 0 (Success)
+# 2 "main.c" 2
 
- */
+
+
+
+
+# 3 "main.c"
 
 int main(void)
-  
+
 {
   
   return (0);
