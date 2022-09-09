5ce18a356768ba6983d3610ca9d4ab15ed4c8a25
Neo Metsing <n.metsing00@gmail.com>
2022-09-09 12:00:32 -0700

    added task 2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+	char alpha[27];
+
+	strcpy(alpha, "abcdefghijklmnopqrstuvwxyz");
+
+	for (i = 0; i < 26; i++)
+		{
+			putchar(alpha[i]);
+		}
+	putchar('\n');
+	return (0);
+}
