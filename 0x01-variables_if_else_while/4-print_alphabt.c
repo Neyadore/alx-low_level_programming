a4b9cc638008fa5f284d3ec9de43523fb5094c52
Neo Metsing <n.metsing00@gmail.com>
2022-09-09 13:17:58 -0700

    added 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char ch;
+
+
+	for (ch = 97; ch <= 122; ch++)
+	{
+		if (ch != 101 || ch != 113)
+		{
+			putchar(ch);
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
