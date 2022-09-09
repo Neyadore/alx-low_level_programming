f818db35cf95cb0d9dec2d8a36329b4ed61804d6
Neo Metsing <n.metsing00@gmail.com>
2022-09-09 11:08:16 -0700

    long fight

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -5,8 +5,11 @@
 /**
  *main - entry point
  *
- *return: Always zero (Success)
-*/
+ *return/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(void)
 {
@@ -14,15 +17,13 @@ int main(void)
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	int a;
-	a = n % 10;
 
-	if (a < 6)
-		printf("The last digit of %i is %i and is greater than 5", n, a);
-	else if (a == 0)
-		printf("The last digit of %i is %i and is zero", n, a);
+	if ((n % 10) > 50)
+		printf("The last digit of %i is %i and is greater than 5", n, (n % 10));
+	else if ((n % 10) == 0)
+		printf("The last digit of %i is %i and is zero", n, (n % 10));
 	else
-		printf("The last digit of %i is %i and less than 6 and is not 0", n, a);
+		printf("The last digit of %i is %i and is less than 6 and not 0", n, (n % 10));
 
 	return (0);
 }
