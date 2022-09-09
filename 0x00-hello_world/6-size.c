83939fb054296bc13c38ea2facf17bb1c5c4717f
Neo Metsing <n.metsing00@gmail.com>
2022-09-09 08:08:50 -0700

    added task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,24 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-	
+	char chartype;
+	int inttype;
+	long int longinttype;
+	long long int longlonginttype;
+	float floattype;
+
+	printf("Size of a char: %zu", sizeof(chartype));
+	printf("Size of an int: %zu", sizeof(inttype));
+	printf("Size of a long int: %zu", sizeof(longinttype));
+	printf("Size of a long long int: %zu", sizeof(longlonginttype));
+	printf("Size of a float: %zu", sizeof(floattype));
+
+	return (0);
+}
