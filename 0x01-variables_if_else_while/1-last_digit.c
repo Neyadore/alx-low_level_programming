7d4b73ee074fdad6606397623744301da221141d
Neo Metsing <n.metsing00@gmail.com>
2022-09-09 11:13:31 -0700

    youth

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -16,13 +16,11 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 
 	if ((n % 10) > 50)
-		printf("The last digit of %i is %i and is greater
- than 5", n, (n % 10));
+		printf("The last digit of %i is %i and is greater than 5", n, (n % 10));
 	else if ((n % 10) == 0)
 		printf("The last digit of %i is %i and is zero", n, (n % 10));
 	else
-		printf("The last digit of %i is %i and is less
- than 6 and not 0", n, (n % 10));
+		printf("The last digit of %i is %i and is less than 6 and not 0", n, (n % 10));
 
 	return (0);
 }
