d8178a994afb9db77873e35cfd85331cb2a32267
Neo Metsing <n.metsing00@gmail.com>
2022-09-09 12:04:57 -0700

    fixed betty

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -17,9 +17,9 @@ int main(void)
 	strcpy(alpha, "abcdefghijklmnopqrstuvwxyz");
 
 	for (i = 0; i < 26; i++)
-		{
-			putchar(alpha[i]);
-		}
+	{
+		putchar(alpha[i]);
+	}
 	putchar('\n');
 	return (0);
 }
