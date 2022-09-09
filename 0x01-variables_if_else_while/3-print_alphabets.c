a2a0b91cfdcd9afbde8a0695d715782455000021
Neo Metsing <n.metsing00@gmail.com>
2022-09-09 12:32:05 -0700

    added 3-print_alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch  = 97 ; ch <= 122; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 65; ch <= 90; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+
+	return (0);
+}
