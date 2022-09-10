36f959598362da39c3a47589e66d62f33bcf4a4d
Neo Metsing <n.metsing00@gmail.com>
2022-09-10 06:59:29 -0700

    added new line to 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,13 +11,16 @@ int main(void)
 {
 	int num;
 
-	for (num = 0; num < 9; num++)
+	for (num = 0; num < 10; num++)
 	{
 		putchar(num + '0');
+		if (num != 9)
+		{
 		putchar(',');
 		putchar(' ');
+		}
 	}
-	putchar('9');
+	putchar('\n');
 
 	return (0);
 }
