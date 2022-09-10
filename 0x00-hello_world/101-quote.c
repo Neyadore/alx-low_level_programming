11fcb77ec8c763678e6620608abb98fb64a26635
Neo Metsing <n.metsing00@gmail.com>
2022-09-10 07:57:40 -0700

    fixed error msg ig

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -11,7 +11,7 @@
 
 int main(void)
 {
-	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59, 1, stderr);
+	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61, 1, stderr);
 	exit(101);
 	return (1);
 }
