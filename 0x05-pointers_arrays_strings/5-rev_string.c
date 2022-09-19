a1ccfb9a37ee310821f08f55deaec1f3faae686e
Neo Metsing <n.metsing00@gmail.com>
2022-09-19 13:20:32 -0700

    tried malloc in 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <string.h>
+#include <stdlib.h>
 
 /**
  * rev_string - reverses a string
@@ -9,13 +10,14 @@
 
 void rev_string(char *s)
 {
-	char* a;
+	char *a;
 	int k;
 	int i;
 	int j = strlen(s);
 
 	for (i = j - 1; i >= 0; i--)
 	{
+		a = (char *)malloc(j * sizeof(char));
 		for (k = 0; k < j; k++)
 		{
 			a[k] = s[i];
