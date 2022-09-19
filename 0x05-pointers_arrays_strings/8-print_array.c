d0efae36e5e704742111603ac1c77829bd97e1fd
Neo Metsing <n.metsing00@gmail.com>
2022-09-19 14:38:31 -0700

    tried task 8

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -14,9 +14,9 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i < (n - 1); i++)
+	printf("%d", a[0]);
+	for (i = 1; i < n; i++)
 	{
-		printf("%d, ", a[i]);
+		printf(", %d", a[i]);
 	}
-	printf("%d", a[n]);
 }
