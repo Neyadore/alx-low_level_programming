bc83d4c21e7a603d0629c86888c8b862637671f2
Neo Metsing <n.metsing00@gmail.com>
2022-09-19 12:54:46 -0700

    added 5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * rev_string - reverses a string
+ *@s: the string
+ *Return: returns nothing
+ */
+
+void rev_string(char *s)
+{
+	char *a;
+	int k;
+	int i;
+	int j = strlen(s);
+
+	for (i = j - 1; i >= 0; i--)
+	{
+		for (k = 0; k < j; k++)
+		{
+			a[k] = s[i];
+		}
+	}
+}
