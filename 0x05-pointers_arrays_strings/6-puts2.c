61ae54d77fcfb8e4477b9bbabcd3fce596b8a8a7
Neo Metsing <n.metsing00@gmail.com>
2022-09-19 14:03:41 -0700

    treid 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ *puts2 - prints every otther hcaracter of a string
+ *@str: the string
+ *Return: returns nothing
+ */
+
+void puts2(char *str)
+{
+	int length = strlen(str);
+	int i;
+	
+	for (i = 0; i < length; i++)
+	{
+		if (length % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}
