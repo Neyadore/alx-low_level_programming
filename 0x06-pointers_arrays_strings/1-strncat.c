8d64b24a42aea26a40d7c2a70675ea6e37356226
Neo Metsing <n.metsing00@gmail.com>
2022-09-22 16:51:54 -0700

    added 1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ *_strncat - concarnates two strings
+ *@dest: destination string
+ *@src: source string
+ *@n: third parameter
+ * Return: a string
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, len;
+
+	while (dest[i++])
+		len++;
+	for (i = 0; src[i] && i < n; i++)
+		dest[len++] = src[i];
+	return (dest);
+}
