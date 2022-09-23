b6332e97b0839eaf5dc8ee433a8d22aac725d3e7
Neo Metsing <n.metsing00@gmail.com>
2022-09-22 17:14:06 -0700

    fixed task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,16 +11,12 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int index;
-	int len = 0;
+	int index = 0, dest_len = 0;
 
 	while (dest[index++])
-	{
-		len++;
-	}
+	dest_len++;
 	for (index = 0; src[index] && index < n; index++)
-	{
-		dest[len++] = src[index];
-	}
+	dest[dest_len++] = src[index];
+
 	return (dest);
 }
