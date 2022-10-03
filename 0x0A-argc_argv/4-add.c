fad99a21d7d4a696bfc5c84f85201b2ef925440f
Neo Metsing <n.metsing00@gmail.com>
2022-10-03 10:49:17 -0700

    update

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ *main - adds posotive integers
+ *@argv: list
+ *@argc: number of args
+ *Return: zero (0) is sucesfull;
+ *(1) if failed
+ */
+
+int main(int argc, char const *argv[])
+{
+	int sum = 0;
+	int i;
+
+	if (argc == 1)
+		printf("0\n");
+
+	for (i = 0; i < argc; i++)
+	{
+		if (isdigit(atoi(argv[i])) == 1)
+		{
+			sum = sum + atoi(argv[i]);
+		}
+	       	else
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+	printf("%i\n", sum);
+	}
