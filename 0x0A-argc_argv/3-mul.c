2f364f0540f7a3268c009f5b03e6a97ddac884bf
Neo Metsing <n.metsing00@gmail.com>
2022-10-03 05:38:22 -0700

    updated

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *main - prints prduct os two numbers
+ *@argc: number
+ *argv: list
+ *Return: Always nothing (0);
+ */
+
+
+int main(int argc, char const *argv[])
+{
+	int product;
+	int a;
+	int b;
+
+	if (argc != 3)
+		return (1);
+	a = argv[1];
+	b = argv[2];
+	product = (a * b);
+	printf("%i\n", product);
+
+	return (0);
+}
