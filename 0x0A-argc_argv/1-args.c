3493d4cee4f705adf2fbf98fb9307289856b1656
Neo Metsing <n.metsing00@gmail.com>
2022-10-03 03:15:51 -0700

    updated

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *main - prints number of arguemnts;
+ *@argv: list of args
+ *@argc: number of args'
+ *Return: Always zero (0)
+ */
+
+int main(int argc, char const *argv[])
+{
+	(void)argv[];
+	printf("%i", argc);
+	return (0);
+}
