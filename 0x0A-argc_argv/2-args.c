0aec16db01df647121e36920e10a45baada1517e
Neo Metsing <n.metsing00@gmail.com>
2022-10-03 03:28:30 -0700

    added 2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *main - printa all arguements with new line
+ *@argv: list
+ *@argc: number;
+ *Return: Always zero (0)
+ */
+
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	int i;
+
+	for (i = 1; argv[i] != NULL; i++)
+	{
+		printf("%s\n", argv[i]);
+		return (0);
+	}
+	
