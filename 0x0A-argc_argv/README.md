b4972ea962c9ae88ee6bda4d8534af721cd533e9
Neo Metsing <n.metsing00@gmail.com>
2022-10-03 02:37:59 -0700

    added 0-whatsmyname.c

---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,2 @@
+this is the readme for this assignment
+0-whatsmyname.c - echos name of function
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int putchar(int);
+int _whatsmyname(char *argv[]);
+
+#endif
